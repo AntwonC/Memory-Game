13c0b84cfaea25aba87daba3b6fc670155a1365d
Anthony Chen <anthonychen33@gmail.com>
2022-07-04 03:33:18 -0400

    Header completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import '../styles/Header.css';
+
+const Header = () => (
+  <div id="headerContainer">
+    <h1 id="headerTitle">Call of Duty Memory Game</h1>
+    <p>Don't click on the same weapon twice!</p>
+  </div>
+);
+
+export default Header;
